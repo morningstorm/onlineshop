238181ad5ed6eb9b4b16ba2436fd8887654be117
morning <977898691@qq.com>
2019-06-24 15:08:55 +0800

    add method display

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,4 +2,7 @@ public class test {
     public static void main(String[] args){
         System.out.println("hello,git");
     }
+    public static void display(){
+        System.out.println("hello,idea");
+    }
 }
